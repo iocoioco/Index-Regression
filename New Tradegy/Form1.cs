e1e0ac367431e02c85d65395ffa0aa3becf7cf61
ilkey <iklee217@gmail.com>
2025-02-22 15:25:24 +0900

    before delete RECT

--- New Tradegy/Form1.cs
+++ New Tradegy/Form1.cs
@@ -302,16 +302,8 @@ namespace New_Tradegy // added for test on 20241020 0300
 
 
 
-        
-        //private void Chart_Paint(object sender, PaintEventArgs e)
-        //{
-        //    if (ChartState.ActiveChart == "chart1")
-        //    {
-        //        ChartHelper.DrawNumbersAtMousePosition(e.Graphics, ChartState.Chart1MousePosition);
-        //    }
-        //}
 
-
+      
 
 
 
