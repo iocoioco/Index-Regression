9c3efa51558da8466ab0f7e4687c760521b83f99
IK LEE <38799839+iocoioco@users.noreply.github.com>
2025-05-06 22:09:09 +0900

    0506 final

--- New Tradegy/Library/Trackers/Charting/ChartBaseRenderer.cs
+++ New Tradegy/Library/Trackers/Charting/ChartBaseRenderer.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace New_Tradegy.Library.Trackers
 {
     internal class ChartBaseRenderer
     {
+        public static void RelocateChartArea(ChartArea area, int row, int col, int nRow, int nCol)
+        {
+            float width = 100f / nCol;
+            float height = 100f / nRow;
+
+            area.Position.X = col * width;
+            area.Position.Y = row * height;
+            area.Position.Width = width;
+            area.Position.Height = height;
+        }
     }
 }
